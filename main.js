aa25ffc757b424caf567609702bd9f5f38cdddcd
prem <prem.sherpa@codeimmersives.com>
2020-10-05 21:53:52 -0400

    write-read file

--- main.js
+++ main.js
@@ -7,3 +7,35 @@
 // - when you create the randomText.txt file you should also log 'randomtext.txt created' in the terminal.
 // - write the randomText.txt data to a new file called verbage.txt inside the content folder and log 'verbage.txt created'
 // - Now create a separate setTimeout function that after 7 seconds deletes the content directory
+console.clear();
+const fs = require('fs');
+
+fs.mkdir('new-directory', (err)=>{
+    if (err) throw err;
+    console.log('folder created');
+})
+
+// creating new file inside the newly created folder and inserting texts inside the file
+fs.writeFile('./randomText.txt','file inside a directory',(err)=>{ 
+    if(err) return console.log(err)
+    console.log('first file created ');
+})
+
+let readme = fs.readFileSync('./randomText.txt', 'utf-8')//will finish this code before moving to next step
+
+
+//creating a file and inserting text form old file
+fs.writeFile('verbage.txt', readme ,(err)=>{
+    if(err) return console.log(err)
+
+     console.log('verbage.txt created')
+})
+
+setTimeout(()=>{
+    fs.rmdir('./new-directory',(err)=>{
+        if(err) return console.log(err)
+
+        console.log('folder deleted')
+    })
+
+}, 7000)
